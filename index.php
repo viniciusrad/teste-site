43cfaf315a9f04ceae32606913de319ed19d6c9c
viniciusrad <36489527+viniciusrad@users.noreply.github.com>
2021-01-22 16:01:50 -0300

    finalização parcial da home

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
             <button class="btn-saiba-mais">Saiba mais</button>
         </div>
         <div class="texto-sobre-imagem">
-            <p style="max-width: 250px;"><strong>IMOBILIÁRIA DIGITAL E CONSUMO 4.0 DISPONÍVEL 24 HORAS POR DIA.</strong></p>
+            <p style="max-width: 260px; padding-left: 3em;"><strong>IMOBILIÁRIA DIGITAL 4.0 DISPONÍVEL 24 HORAS POR DIA.</strong></p>
 
         </div>
     </div>
@@ -150,6 +150,7 @@
     <div class="div-btn-proposta">
         <button class="btn-saiba-mais-black">Quero receber uma proposta</button>
     </div>
+
     <div class="div-fundo-cinza">
         <p>O ÚNICO CRM COM GESTÃO DE LEADS NO MERCADO IMOBILIÁRIO</p>
     </div>
@@ -175,7 +176,127 @@
     <div class="div-fundo-cinza">
         <p>SITES COMPROVADAMENTE COM OS MELHORES POSICIONAMENTOS NA BUSCA ORGÂNICA (GRATUITA) DO GOOGLE</p>
     </div>
+    <!-- area de depoimentos -->
+    <!-- <div class="div-depoimentos">
+        <div class="card-depoimentos">
+            <div class="depoimentos-header">
+                <img src="https://via.placeholder.com/150" alt="">
+                <div>
+                    <p>Miliete</p>
+                    <p>Corretora autônoma</p>
+                </div>
 
+            </div>
+            <div class="depoimentos-body">
+                <p>Quando comecei a trabalhar como profissional autônoma, queria me profissionalizar, no ramo Imobiliário e estar nos melhores portais. Então não tive dúvidas, escolhi o melhor! Midas. Os profissionais desse sistema, são atenciosos e realmente resolvem sempre o que precisamos. Neste ramo, não podemos perder tempo e o Midas, nós dá um excelente suporte. Parabéns a equipe Midas. </p>
+            </div>
+        </div>
+    </div> -->
+
+    <!--  -->
+    <!--  -->
+    <div class="area-depoimentos">
+
+        <h3>Casos de sucesso</h3>
+
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+
+
+            <div class="carousel-inner" role="listbox">
+
+
+                <!-- Slide 1 -->
+                <div class="carousel-item active">
+                    <div class="div-depoimentos">
+                        <div class="card-depoimentos">
+                            <div class="depoimentos-header">
+                                <img src="https://via.placeholder.com/150" alt="">
+                                <div>
+                                    <p>Miliete</p>
+                                    <p>Corretora autônoma</p>
+                                </div>
+
+                            </div>
+                            <div class="depoimentos-body">
+                                <p>Quando comecei a trabalhar como profissional autônoma, queria me profissionalizar, no ramo Imobiliário e estar nos melhores portais. Então não tive dúvidas, escolhi o melhor! Midas. Os profissionais desse sistema, são atenciosos e realmente resolvem sempre o que precisamos. Neste ramo, não podemos perder tempo e o Midas, nós dá um excelente suporte. Parabéns a equipe Midas. </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- Slide 2-->
+                <div class="carousel-item">
+                    <div class="div-depoimentos">
+                        <div class="card-depoimentos">
+                            <div class="depoimentos-header">
+                                <img src="https://via.placeholder.com/150" alt="">
+                                <div>
+                                    <p>Karine</p>
+                                    <p>Solidez Imobiliária</p>
+                                </div>
+
+                            </div>
+                            <div class="depoimentos-body">
+                                <p>Minhas impressões com relação ao sistema não poderia ser melhor. Elenco os pontos positivos: </p>
+                                <ul>
+                                    <li>1 - Sistema de fácil manuseio, auto explicativo.</li>
+                                    <li>2- Completo e integrado a diversos portais o que ajuda a maximiza o tempo.</li>
+                                    <li>3- Manual com passo a passo disponível 24h no campo: ajuda.</li>
+                                    <li>4- Suporte por telefone e dentro do sistema.</li>
+                                </ul>
+                                <p>Elenco pontos negativos: Nenhum!</p>
+                                <p>Enfim, o sistema é maravilhoso, o atendimento é excelente, de toda equipe como: você, Tania do administrativo e Cássio do site, estou super satisfeita com a empresa e recomendo a todos.
+                                    Muito obrigada!</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Slide 3-->
+                <div class="carousel-item">
+                    <div class="div-depoimentos">
+                        <div class="card-depoimentos">
+                            <div class="depoimentos-header">
+                                <img src="https://via.placeholder.com/150" alt="">
+                                <div>
+                                    <p>Miliete</p>
+                                    <p>Corretora autônoma</p>
+                                </div>
+
+                            </div>
+                            <div class="depoimentos-body">
+                                <p>Quando comecei a trabalhar como profissional autônoma, queria me profissionalizar, no ramo Imobiliário e estar nos melhores portais. Então não tive dúvidas, escolhi o melhor! Midas. Os profissionais desse sistema, são atenciosos e realmente resolvem sempre o que precisamos. Neste ramo, não podemos perder tempo e o Midas, nós dá um excelente suporte. Parabéns a equipe Midas. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!-- indicadores laterais do carrossel de imagens -->
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="span-carrossel" aria-hidden="true">
+                    < </span>
+                        <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="span-carrossel" aria-hidden="true"> > </span>
+                <span class="sr-only">Next</span>
+            </a>
+
+
+        </div>
+    </div>
+
+    <!--  -->
+    <!--  -->
+
+    <!-- fimm da area de depoimentos -->
+
+    <!-- area de contato -->
     <div class="form-contato">
         <div class="div-texto-formulario">
             <p>O FUTURO DA SUA IMOBILIÁRIA COMEÇA AGORA!</p>
