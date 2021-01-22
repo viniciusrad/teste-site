45a23d7b9cbd15c04c9219bf2b81ad89e3f1824d
viniciusrad <36489527+viniciusrad@users.noreply.github.com>
2021-01-22 09:29:35 -0300

    commit inicial - home quase prota

---
+++ header.php
@@ -0,0 +1,135 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="img/logo-fav-icon.png">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Comfortaa%7CComfortaa%7CComfortaa%7CComfortaa&amp;subset=latin-ext,cyrillic-ext,latin,vietnamese,cyrillic,greek" media="all">
+
+    <title>Midas mais</title>
+</head>
+
+
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+        <div class="container">
+
+
+            <div class="entrada-topo entrada-topo-scroll mover" style="display: flex;">
+
+                <!-- sanduiche das paginas midas -->
+                <div class="btn-group" role="group">
+                    <button id="btnGroupDrop1" type="button" class="btn btn-light dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <a href="javascript:void(0);" class="icon"><img src="./img/menu-r.png" alt="menu" title="menu" class="pull-left sanduiche"></a>
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                        <a class="dropdown-item" href="administracaoImoveis.php" style="border-left: 7px solid #d93117;margin-left: 10px;">SISTEMA PARA IMOBILIÁRIA</a>
+                        <a class="dropdown-item" href="siteImobiliarias.php" style="border-left: 7px solid #ffb017;margin-left: 10px;"> SITE PARA IMOBILIÁRIA</a>
+                        <a class="dropdown-item" href="sistemaImobiliarias.php" style="border-left: 7px solid #7434d3;margin-left: 10px;">MARKETING DIGITAL PARA IMOBLIÁRIA</a>
+                        <a class="dropdown-item" href="chatbotImobiliarias.php" style="border-left: 7px solid #9c136f;margin-left: 10px;">CHATBOT</a>
+                        <a class="dropdown-item" href="administracaoImoveis.php" style="border-left: 7px solid #0877ac;margin-left: 10px;">ADMINISTRACAO DE IMOVEIS</a>
+                        <a class="dropdown-item" href="#" style="border-left: 7px solid #4caa30;margin-left: 10px;">VISITA VIRTUAL</a>
+                        <a class="dropdown-item" href="#" style="border-left: 7px solid #0877ac;margin-left: 10px;">QUEM SOMOS</a>
+                        <a class="dropdown-item" href="#" style="border-left: 7px solid #666;margin-left: 10px;">BLOG</a>
+                    </div>
+                </div>
+                <!-- sanduiche das paginas midas -->
+
+                <div class="pull-left"><a href="https://www.midasmais.com.br/novo_site"><img id="img-logo" src="./img/logo-midas2.png" alt="Midas" title="Midas"></a></div>
+            </div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="" href="#"><i class="fas fa-camera"></i>
+                            <label for="icon-facebook" class='label-nav-icon'>Instagran</label>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="" href="#"><i class="fab fa-facebook"></i>
+                            <label for="icon-facebook" class='label-nav-icon'>Facebook</label>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="" href="#"><i class="fas fa-phone-alt"></i>
+                            <label for="icon-facebook" class='label-nav-icon'>contato</label>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="" href="#"><i class="fas fa-unlock-alt"></i> Área do cliente</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </nav>
+    <!-- Navigation -->
+
+
+</header>
+
+<script>
+    // The debounce function receives our function as a parameter
+    const debounce = (fn) => {
+
+        // This holds the requestAnimationFrame reference, so we can cancel it if we wish
+        let frame;
+
+        // The debounce function returns a new function that can receive a variable number of arguments
+        return (...params) => {
+
+            // If the frame variable has been defined, clear it now, and queue for next frame
+            if (frame) {
+                cancelAnimationFrame(frame);
+            }
+
+            // Queue our function call for the next frame
+            frame = requestAnimationFrame(() => {
+
+                fn(...params);
+            });
+
+        }
+
+    };
+
+
+    // Reads out the scroll position and stores it in the data attribute
+    // so we can use it in our stylesheets
+    const storeScroll = () => {
+        document.documentElement.dataset.scroll = window.scrollY;
+
+        if (window.scrollY != 0) {
+
+            $("#img-logo").attr("src", "img/Logo-Midas-Mais-branca.png");
+        } else {
+            $("#img-logo").attr("src", "img/logo-midas2.png");
+
+        }
+    }
+
+    // Listen for new scroll events, here we debounce our `storeScroll` function
+    document.addEventListener('scroll', debounce(storeScroll), {
+        passive: true
+    });
+
+    // Update scroll position for first time
+    storeScroll();
+
+
+    $(document).ready(function() {
+
+
+
+    })
+</script>
---
+++ index.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<?php include('header.php') ?>
+
+<body>
+
+    <!-- Imagem principal -->
+    <div class="imagem-principal">
+        <div class="texto-imagem-principal">
+            <h2>IMOBILIÁRIA NA NUVEM?</h2>
+            <p>A Tecnologia Corporativa mais importante e inovadora disponível para Imobiliárias e Corretores.</p>
+            <button class="btn-saiba-mais">Saiba mais</button>
+        </div>
+        <div class="texto-sobre-imagem">
+            <p style="max-width: 250px;"><strong>IMOBILIÁRIA DIGITAL E CONSUMO 4.0 DISPONÍVEL 24 HORAS POR DIA.</strong></p>
+
+        </div>
+    </div>
+    <div class="imagem-principal-mobile">
+        <img src="img/corretorNuvem.png" alt="">
+        <div>
+            <h3>IMOBILIÁRIA NA NUVEM?</h3>
+            <p>A Tecnologia Corporativa mais importante e inovadora disponível para Imobiliárias e Corretores.</p>
+            <button class="btn-saiba-mais-black">Saiba mais</button>
+
+        </div>
+    </div>
+    <!-- espaço do crm -->
+    <div class="div-crm">
+        <div class="div-img-crm">
+            <img src="img/crm-screen-shots.png" alt="">
+        </div>
+        <div class="lista-crm">
+            <h2>CRM EXCLUSIVO PARA IMOBILIÁRIAS E CORRETORES</h2>
+            <ul>
+                <li>Integração com Portais;</li>
+                <li>Integração Facebook e Whatsapp;</li>
+                <li>Funil de Vendas;</li>
+                <li>Análise de Retorno;</li>
+                <li>Transferência Automática de Clientes;</li>
+                <li>Gestão total da sua Equipe;</li>
+                <li>E-mails Automáticos.</li>
+            </ul>
+            <button class="btn-saiba-mais-black">Saiba mais</button>
+
+        </div>
+    </div>
+
+    <hr>
+
+    <div class="align-text">
+        <h3>FUNCIONALIDADES</h3>
+    </div>
+    <!-- Cards de seriços -->
+    <div class="container container-icones">
+        <div class="card-icone">
+            <a href="sistemaImobiliarias.php">
+                <div class="div-icone">
+                    <img class="img-icone" src="img/icone-sistema-para-imobiliarias.png" alt="Avatar">
+                </div>
+                <div class="container">
+                    <p class="textos-icones">Sistemas para imobiliaria</p>
+                </div>
+            </a>
+        </div>
+        <div class="card-icone">
+            <a href="siteImobiliarias.php">
+                <div class="div-icone">
+                    <img class="img-icone" src="img/icone-site-para-imobiliarias.png" alt="Avatar">
+                </div>
+                <div class="container">
+                    <p class="textos-icones">Site para imobiliarias</p>
+                </div>
+            </a>
+        </div>
+
+        <div class="card-icone">
+            <a href="administracaoImoveis.php">
+                <div class="div-icone">
+                    <img class="img-icone" src="img/icone-locação-digital.png" alt="Avatar">
+                </div>
+                <div class="container">
+                    <p class="textos-icones">Locação digital</p>
+                </div>
+            </a>
+        </div>
+
+        <div class="card-icone">
+            <a href="administracaoImoveis.php">
+                <div class="div-icone">
+                    <img class="img-icone" src="img/icone-administracao-imoveis.png" alt="Avatar">
+                </div>
+                <div class="container">
+                    <p class="textos-icones">administração de imóveis</p>
+                </div>
+            </a>
+        </div>
+
+        <div class="card-icone">
+            <a href="administracaoImoveis.php">
+                <div class="div-icone">
+                    <img class="img-icone" src="img/icone-remanescentes-de-lancamentos.png" alt="Avatar">
+                </div>
+                <div class="container">
+                    <p class="textos-icones">Remanescentes de lançamentos</p>
+                </div>
+            </a>
+        </div>
+
+        <div class="card-icone">
+            <a href="administracaoImoveis.php">
+                <div class="div-icone">
+                    <img class="img-icone" src="img/icone-mql-lancamentos-de-leads.png" alt="Avatar">
+                </div>
+                <div class="container">
+                    <p class="textos-icones">MQL Qualificação de leads</p>
+                </div>
+            </a>
+        </div>
+
+
+        <div class="card-icone">
+            <a href="chatbotImobiliarias.php">
+                <div class="div-icone">
+                    <img class="img-icone" src="img/icone-chatbot.png" alt="Avatar">
+                </div>
+                <div class="container">
+                    <p class="textos-icones">Chatbot para imobiliária</p>
+                </div>
+            </a>
+        </div>
+
+        <div class="card-icone">
+            <a href="visitaImoveis.php">
+                <div class="div-icone">
+                    <img class="img-icone" src="img/icone-360.png" alt="Avatar">
+                </div>
+                <div class="container">
+                    <p class="textos-icones">Visita virtual</p>
+                </div>
+            </a>
+        </div>
+
+    </div>
+    <!-- fim do container de Cards de seriços -->
+
+    <div class="div-btn-proposta">
+        <button class="btn-saiba-mais-black">Quero receber uma proposta</button>
+    </div>
+    <div class="div-fundo-cinza">
+        <p>O ÚNICO CRM COM GESTÃO DE LEADS NO MERCADO IMOBILIÁRIO</p>
+    </div>
+
+    <div class="div-crm">
+        <div class="lista-crm">
+            <h2>SITE EXCLUSIVO PARA IMOBILIÁRIAS E CORRETORES</h2>
+            <ul>
+                <li>SEO – Otimização para o Google;</li>
+                <li>Layouts Responsivos;</li>
+                <li>Busca de avançada de Imóveis;</li>
+                <li>Alta Performance;</li>
+                <li>Usabilidade e Aparência;</li>
+                <li>Autonomia e Flexibilidade no Gerenciamento do Conteúdo!</li>
+            </ul>
+            <button class="btn-saiba-mais-black">Saiba mais</button>
+        </div>
+        <div class="div-img-crm">
+            <img src="img/site-responsivo.jpg" alt="">
+        </div>
+    </div>
+
+    <div class="div-fundo-cinza">
+        <p>SITES COMPROVADAMENTE COM OS MELHORES POSICIONAMENTOS NA BUSCA ORGÂNICA (GRATUITA) DO GOOGLE</p>
+    </div>
+
+    <div class="form-contato">
+        <div class="div-texto-formulario">
+            <p>O FUTURO DA SUA IMOBILIÁRIA COMEÇA AGORA!</p>
+        </div>
+        <div class="div-formulario">
+            <p>Preencha o formulário e receba uma oferta exclusiva.</p>
+            <form action="">
+                <div class="div-field">
+                    <label for="nome">Nome Completo<span style="color: red;">*</span></label>
+                    <input type="text" name="nome" id="input-nome">
+                </div>
+                <div class="div-field">
+                    <label for="imobiliaria">Imobiliária<span style="color: red;">*</span></label>
+                    <input type="text" name="imobiliaria" id="input-imobiliaria">
+                </div>
+                <div class="div-field">
+                    <label for="e-mail">E-mail<span style="color: red;">*</span></label>
+                    <input type="text" name="e-mail" id="input-e-mail">
+                </div>
+                <div class="div-field">
+                    <label for="telefone">Telefone<span style="color: red;">*</span></label>
+                    <input type="text" name="telefone" id="input-telefone">
+                </div>
+                <div class="div-field">
+                    <label for="mensagem">Mensagem<span style="color: red;">*</span></label>
+                    <textarea rows="4" name="mensagem" id="input-mensagem"></textarea>
+                </div>
+
+            </form>
+        </div>
+    </div>
+
+    <div class="rodape">
+        <div>
+            <h3 class="texto-centralizado">MIDAS MAIS</h3>
+            <p class="texto-centralizado">As soluções MIDAS vem deixando sua marca de sucesso em cada uma das Empresas que procuram utilizar o máximo da tecnologia em favor dos seus negócios.</p>
+        </div>
+    </div>
+
+
+
+    <!--  -->
+    <!--  -->
+    <!--  -->
+    <!-- Modal -->
+    <div class="modal fade" id="modalBaner" tabindex="-1" role="dialog" aria-labelledby="modalBanerLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalBanerLabel">Titulo da campanha</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <img src="img/fundo2.jpg" alt="">
+                </div>
+                <div class=" modal-footer">
+                    <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button> -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script type="text/javascript">
+        /**Ativa o modal */
+        // $(window).on('load', function() {
+        //     $('#modalBaner').modal('show');
+        // });
+    </script>
+</body>
+
+
+
+</html>
