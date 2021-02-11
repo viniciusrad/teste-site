bdb7ab3822bcd0870ef603ad651c2825468a7b6c
viniciusrad <36489527+viniciusrad@users.noreply.github.com>
2021-02-11 09:44:36 -0300

    imagens dos icones

---
+++ siteImobiliarias.php
@@ -0,0 +1,383 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<?php include('header.php') ?>
+
+<body>
+
+
+
+    <div class="div-fundo-verde">
+        <p>Excelente ranqueamento no Google. Resultados comprovados!</p>
+    </div>
+
+    <div class="div-crm">
+        <div class="div-img-crm">
+            <img src="img/modelo-urano.png" alt="">
+        </div>
+        <div class="texto-crm">
+            <p><strong>Se você busca escrever uma história de sucesso na web e quer que o Google conheça seu negócio, você precisa ter um site.</strong></p>
+            <p>Se você não tem um site para receber as pessoas que estão buscando pelo que você oferece, os seus potenciais clientes supõem que você não tem nome no mercado, não tem credibilidade, que é um negócio amador.</p>
+            <p>Se você não tem um site para receber as pessoas que estão buscando pelo que você oferece, os seus potenciais clientes supõem que você não tem nome no mercado, não tem credibilidade, que é um negócio amador.</p>
+            </br>
+            <p><strong>Se estiver se perguntando: “Com tantas redes sociais, ainda é importante ter um site?” Seguramente a resposta é SIM! E MUITO!</strong></p>
+
+        </div>
+    </div>
+
+    <div class="div-texto-centralizado">
+        <p>Outra questão importante para considerar é que sem um site com domínio próprio, é absolutamente ineficaz realizar campanhas na web. Afinal, depois de atrair, para onde você vai levar seus leads?</p>
+    </div>
+    <div class="div-texto-centralizado">
+        <p><strong>Os SITES MIDAS foram desenvolvidos exclusivamente para o mercado imobiliário e vêm deixando sua marca de sucesso para cada uma das empresas que procuram utilizar o máximo da tecnologia a favor de seus negócios.</strong></p>
+    </div>
+
+    <div class="div-fundo-verde">
+        <p>Excelente ranqueamento no Google. Resultados comprovados!</p>
+    </div>
+
+    <div class="div-check-dupla">
+        <div>
+
+            <div class="item-check-list-site">
+                <div class="div-img-check-dupla">
+                    <div class="    ">
+                        <img src="./img/check-icon.png" alt="">
+                    </div>
+                </div>
+                <div class="texto-check-site">
+                    <p>Desenvolvido por profissionais experientes, pensado para o uso exclusivo de Imobiliárias e Corretores de Imóveis.</p>
+                </div>
+            </div>
+            <div class="item-check-list-site">
+                <div class="div-img-check-dupla">
+                    <div class="    ">
+                        <img src="./img/check-icon.png" alt="">
+                    </div>
+                </div>
+                <div class="texto-check-site">
+                    <p>Otimizado para o Google com as mais avançadas técnicas de SEO. Chances reais de ser bem ranqueado nas buscas orgânicas.</p>
+                </div>
+            </div>
+            <div class="item-check-list-site">
+                <div class="div-img-check-dupla">
+                    <div class="    ">
+                        <img src="./img/check-icon.png" alt="">
+                    </div>
+                </div>
+                <div class="texto-check-site">
+                    <p>Totalmente gerenciável. Você pode criar novas páginas, editar dados e imagens sem depender de terceiros.</p>
+                </div>
+            </div>
+            <div class="item-check-list-site">
+                <div class="div-img-check-dupla">
+                    <div class="    ">
+                        <img src="./img/check-icon.png" alt="">
+                    </div>
+                </div>
+                <div class="texto-check-site">
+                    <p>Modelos Responsivos, com visual contemporâneo que valoriza seu anúncio.</p>
+                </div>
+            </div>
+        </div>
+        <div>
+
+            <div class="item-check-list-site">
+                <div class="div-img-check-dupla">
+                    <div class="    ">
+                        <img src="./img/check-icon.png" alt="">
+                    </div>
+                </div>
+                <div class="texto-check-site">
+                    <p>Boa performance e usabilidade.</p>
+                </div>
+            </div>
+            <div class="item-check-list-site">
+                <div class="div-img-check-dupla">
+                    <div class="    ">
+                        <img src="./img/check-icon.png" alt="">
+                    </div>
+                </div>
+                <div class="texto-check-site">
+                    <p>Integração com Redes Sociais, Portais Imobiliários e Whatsapp, propagação rápida e fácil de anúncios.</p>
+                </div>
+            </div>
+            <div class="item-check-list-site">
+                <div class="div-img-check-dupla">
+                    <div class="    ">
+                        <img src="./img/check-icon.png" alt="">
+                    </div>
+                </div>
+                <div class="texto-check-site">
+                    <p>Acompanhamento de Resultados através do Google Analytics</p>
+                </div>
+            </div>
+            <div class="item-check-list-site">
+                <div class="div-img-check-dupla">
+                    <div class="    ">
+                        <img src="./img/check-icon.png" alt="">
+                    </div>
+                </div>
+                <div class="texto-check-site">
+                    <p>Certificado SSL incluso</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="div-fundo-verde">
+        <p>CONFIRA NOSSOS MODELOS</p>
+    </div>
+
+
+
+
+
+    <!-- //////////////////***************************//////////////////// */ -->
+    <!-- //////////////////*****carrossel modelos*****//////////////////// */ -->
+    <!-- //////////////////***************************//////////////////// */ -->
+
+
+    <div id="area-carrosel-modelos" class="area-depoimentos">
+
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+            </ol>
+
+
+            <div id="carrossel-modelos-site" class="carousel-inner" role="listbox">
+
+
+                <!-- Slide 1 -->
+                <div class="carousel-item active">
+                    <img src="img/modelo-urano.png" class="img-fluid" alt="">
+                </div>
+                <!-- Slide 2-->
+                <div class="carousel-item">
+                    <img src="img/modelo-jupiter.png" class="img-fluid" alt="">
+                </div>
+                <!-- Slide 3-->
+                <div class="carousel-item">
+                    <img src="img/modelo-saturno.png" class="img-fluid" alt="">
+                </div>
+                <!-- Slide 5-->
+                <div class="carousel-item">
+                    <img src="img/modelo-plutao.png" class="img-fluid" alt="">
+                </div>
+                <!-- Slide 5-->
+                <div class="carousel-item">
+                    <img src="img/modelo-venus.png" class="img-fluid" alt="">
+                </div>
+                <!-- Slide 6-->
+                <div class="carousel-item">
+                    <img src="img/modelo-marte.png" class="img-fluid" alt="">
+                </div>
+
+            </div>
+            <!-- indicadores laterais do carrossel de imagens -->
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="span-carrossel" aria-hidden="true"> <img src="img/seta-esquerda.png" class="img-fluid" alt=""> </span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="span-carrossel" aria-hidden="true"> <img src="img/seta-direita.png" class="img-fluid" alt=""> </span>
+                <span class="sr-only">Next</span>
+            </a>
+
+
+        </div>
+    </div>
+
+
+    <!-- //////////////////***************************//////////////////// */ -->
+    <!-- //////////////////***************************//////////////////// */ -->
+    <!-- area de depoimentos -->
+
+    <div class="area-depoimentos">
+
+        <h3>Casos de sucesso</h3>
+
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+
+
+            <div class="carousel-inner" role="listbox">
+
+
+                <!-- Slide 1 -->
+                <div class="carousel-item active">
+                    <div class="div-depoimentos">
+                        <div class="card-depoimentos">
+                            <div class="depoimentos-header">
+                                <img src="https://via.placeholder.com/150" alt="">
+                                <div>
+                                    <p>Miliete</p>
+                                    <p>Corretora autônoma</p>
+                                </div>
+
+                            </div>
+                            <div class="depoimentos-body">
+                                <p>Quando comecei a trabalhar como profissional autônoma, queria me profissionalizar, no ramo Imobiliário e estar nos melhores portais. Então não tive dúvidas, escolhi o melhor! Midas. Os profissionais desse sistema, são atenciosos e realmente resolvem sempre o que precisamos. Neste ramo, não podemos perder tempo e o Midas, nós dá um excelente suporte. Parabéns a equipe Midas. </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- Slide 2-->
+                <div class="carousel-item">
+                    <div class="div-depoimentos">
+                        <div class="card-depoimentos">
+                            <div class="depoimentos-header">
+                                <img src="https://via.placeholder.com/150" alt="">
+                                <div>
+                                    <p>Karine</p>
+                                    <p>Solidez Imobiliária</p>
+                                </div>
+
+                            </div>
+                            <div class="depoimentos-body">
+                                <p>Minhas impressões com relação ao sistema não poderia ser melhor. Elenco os pontos positivos: </p>
+                                <ul>
+                                    <li>1 - Sistema de fácil manuseio, auto explicativo.</li>
+                                    <li>2- Completo e integrado a diversos portais o que ajuda a maximiza o tempo.</li>
+                                    <li>3- Manual com passo a passo disponível 24h no campo: ajuda.</li>
+                                    <li>4- Suporte por telefone e dentro do sistema.</li>
+                                </ul>
+                                <p>Elenco pontos negativos: Nenhum!</p>
+                                <p>Enfim, o sistema é maravilhoso, o atendimento é excelente, de toda equipe como: você, Tania do administrativo e Cássio do site, estou super satisfeita com a empresa e recomendo a todos.
+                                    Muito obrigada!</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Slide 3-->
+                <div class="carousel-item">
+                    <div class="div-depoimentos">
+                        <div class="card-depoimentos">
+                            <div class="depoimentos-header">
+                                <img src="https://via.placeholder.com/150" alt="">
+                                <div>
+                                    <p>Miliete</p>
+                                    <p>Corretora autônoma</p>
+                                </div>
+
+                            </div>
+                            <div class="depoimentos-body">
+                                <p>Quando comecei a trabalhar como profissional autônoma, queria me profissionalizar, no ramo Imobiliário e estar nos melhores portais. Então não tive dúvidas, escolhi o melhor! Midas. Os profissionais desse sistema, são atenciosos e realmente resolvem sempre o que precisamos. Neste ramo, não podemos perder tempo e o Midas, nós dá um excelente suporte. Parabéns a equipe Midas. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!-- indicadores laterais do carrossel de imagens -->
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="span-carrossel" aria-hidden="true">
+                    < </span>
+                        <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="span-carrossel" aria-hidden="true"> > </span>
+                <span class="sr-only">Next</span>
+            </a>
+
+
+        </div>
+    </div>
+
+    <!--  -->
+    <!--  -->
+
+    <!-- fimm da area de depoimentos -->
+
+    <!-- area de contato -->
+    <div class="form-contato">
+        <div class="div-texto-formulario">
+            <p>O FUTURO DA SUA IMOBILIÁRIA COMEÇA AGORA!</p>
+        </div>
+        <div class="div-formulario">
+            <p>Preencha o formulário e receba uma oferta exclusiva.</p>
+            <form action="">
+                <div class="div-field">
+                    <label for="nome">Nome Completo<span style="color: red;">*</span></label>
+                    <input type="text" name="nome" id="input-nome">
+                </div>
+                <div class="div-field">
+                    <label for="imobiliaria">Imobiliária<span style="color: red;">*</span></label>
+                    <input type="text" name="imobiliaria" id="input-imobiliaria">
+                </div>
+                <div class="div-field">
+                    <label for="e-mail">E-mail<span style="color: red;">*</span></label>
+                    <input type="text" name="e-mail" id="input-e-mail">
+                </div>
+                <div class="div-field">
+                    <label for="telefone">Telefone<span style="color: red;">*</span></label>
+                    <input type="text" name="telefone" id="input-telefone">
+                </div>
+                <div class="div-field">
+                    <label for="mensagem">Mensagem<span style="color: red;">*</span></label>
+                    <textarea rows="4" name="mensagem" id="input-mensagem"></textarea>
+                </div>
+
+            </form>
+        </div>
+    </div>
+
+    <div class="rodape">
+        <div>
+            <h3 class="texto-centralizado">MIDAS MAIS</h3>
+            <p class="texto-centralizado">As soluções MIDAS vem deixando sua marca de sucesso em cada uma das Empresas que procuram utilizar o máximo da tecnologia em favor dos seus negócios.</p>
+        </div>
+    </div>
+
+
+
+    <!--  -->
+    <!--  -->
+    <!--  -->
+    <!-- Modal -->
+    <div class="modal fade" id="modalBaner" tabindex="-1" role="dialog" aria-labelledby="modalBanerLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalBanerLabel">Titulo da campanha</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <img src="img/fundo2.jpg" alt="">
+                </div>
+                <div class=" modal-footer">
+                    <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button> -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script type="text/javascript">
+        /**Ativa o modal */
+        // $(window).on('load', function() {
+        //     $('#modalBaner').modal('show');
+        // });
+    </script>
+</body>
+
+
+
+</html>
