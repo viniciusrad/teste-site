7593bbdf1027b8f20b12d478554660694d6771b5
viniciusrad <36489527+viniciusrad@users.noreply.github.com>
2021-02-11 12:03:31 -0300

    pagina administracao de imoveis criada

---
+++ locacaoDigital.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<?php include('header.php') ?>
+
+<body>
+
+
+
+    <div class="div-fundo-verde">
+        <p>LOCAÇÃO DIGITAL:</p>
+        <p>dê o primeiro passo para digitalização da sua imobiliária</p>
+    </div>
+
+    <div class="div-crm">
+        <div class="div-img-crm" style="max-width: 1100px">
+            <img src="img/esteira-locacao-digital.png" alt="">
+        </div>
+    </div>
+
+    <div class="div-texto-centralizado">
+        <p><strong>Um processo de locação sem desgastes. O locador sabe exatamente em que etapa o processo se encontra. O locatário consegue enviar documentos pelo celular ou computador. A imobiliária tem maior controle sobre tudo e consegue cortar gastos.</strong></p>
+    </div>
+
+    <div class="div-fundo-verde">
+        <p>AGILIDADE NAS TRANSAÇÕES IMOBILIÁRIAS</p>
+
+    </div>
+
+    <!-- area de funcionalidades -->
+    <div class="div-funcionalidades-flex">
+        <div class="card-funcionalidade">
+            <div class="div-icone-check">
+                <img class="img-icone-check" src="img/icone-loc-crm.png" alt="Avatar">
+            </div>
+            <div class="container container-check">
+                <p class="textos-icones-check">Compátivel com nosso CRM e uso totalmente integrado.</p>
+            </div>
+        </div>
+        <div class="card-funcionalidade">
+            <div class="div-icone-check">
+                <img class="img-icone-check" src="img/icone-loc-proposta.png" alt="Avatar">
+            </div>
+            <div class="container container-check">
+                <p class="textos-icones-check">O envio dos documentos de forma facilitada é o primeiro passo até o fechamento de contrato.</p>
+            </div>
+        </div>
+        <div class="card-funcionalidade">
+            <div class="div-icone-check">
+                <img class="img-icone-check" src="img/icone-loc-analise.png" alt="Avatar">
+            </div>
+            <div class="container container-check">
+                <p class="textos-icones-check">A análise de crédito é sempre um momento decisivo, e a experiência do cliente vale</p>
+            </div>
+        </div>
+        <div class="card-funcionalidade">
+            <div class="div-icone-check">
+                <img class="img-icone-check" src="img/icone-loc-garantia.png" alt="Avatar">
+            </div>
+            <div class="container container-check">
+                <p class="textos-icones-check">Acesso a soluções para contratação de garantia, totalmente online e com muita segurança.</p>
+            </div>
+        </div>
+        <div class="card-funcionalidade">
+            <div class="div-icone-check">
+                <img class="img-icone-check" src="img/icone-loc-vistoria.png" alt="Avatar">
+            </div>
+            <div class="container container-check">
+                <p class="textos-icones-check">Solicite vistorias imobiliárias e acompanhe o progresso da atividade.</p>
+            </div>
+        </div>
+        <div class="card-funcionalidade">
+            <div class="div-icone-check">
+                <img class="img-icone-check" src="img/icone-loc-contrato.png" alt="Avatar">
+            </div>
+            <div class="container container-check">
+                <p class="textos-icones-check">A análise de crédito é sempre um momento decisivo, e a experiência do cliente vale muitos pontos nesse sentido.</p>
+            </div>
+        </div>
+        <div class="card-funcionalidade">
+            <div class="div-icone-check">
+                <img class="img-icone-check" src="img/icone-loc-erp.png" alt="Avatar">
+            </div>
+            <div class="container container-check">
+                <p class="textos-icones-check">Integramos nossa plataforma com o ERP do seu dia-a-dia.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="div-texto-centralizado">
+        <a href="./contato.php">
+            <button class="btn-saiba-mais-green">QUERO DIGITALIZAR</button>
+        </a>
+    </div>
+    <hr />
+
+
+
+    <!-- area de depoimentos -->
+
+    <div class="area-depoimentos">
+
+        <h3>Casos de sucesso</h3>
+
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+
+
+            <div class="carousel-inner" role="listbox">
+
+
+                <!-- Slide 1 -->
+                <div class="carousel-item active">
+                    <div class="div-depoimentos">
+                        <div class="card-depoimentos">
+                            <div class="depoimentos-header">
+                                <img src="https://via.placeholder.com/150" alt="">
+                                <div>
+                                    <p>Miliete</p>
+                                    <p>Corretora autônoma</p>
+                                </div>
+
+                            </div>
+                            <div class="depoimentos-body">
+                                <p>Quando comecei a trabalhar como profissional autônoma, queria me profissionalizar, no ramo Imobiliário e estar nos melhores portais. Então não tive dúvidas, escolhi o melhor! Midas. Os profissionais desse sistema, são atenciosos e realmente resolvem sempre o que precisamos. Neste ramo, não podemos perder tempo e o Midas, nós dá um excelente suporte. Parabéns a equipe Midas. </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- Slide 2-->
+                <div class="carousel-item">
+                    <div class="div-depoimentos">
+                        <div class="card-depoimentos">
+                            <div class="depoimentos-header">
+                                <img src="https://via.placeholder.com/150" alt="">
+                                <div>
+                                    <p>Karine</p>
+                                    <p>Solidez Imobiliária</p>
+                                </div>
+
+                            </div>
+                            <div class="depoimentos-body">
+                                <p>Minhas impressões com relação ao sistema não poderia ser melhor. Elenco os pontos positivos: </p>
+                                <ul>
+                                    <li>1 - Sistema de fácil manuseio, auto explicativo.</li>
+                                    <li>2- Completo e integrado a diversos portais o que ajuda a maximiza o tempo.</li>
+                                    <li>3- Manual com passo a passo disponível 24h no campo: ajuda.</li>
+                                    <li>4- Suporte por telefone e dentro do sistema.</li>
+                                </ul>
+                                <p>Elenco pontos negativos: Nenhum!</p>
+                                <p>Enfim, o sistema é maravilhoso, o atendimento é excelente, de toda equipe como: você, Tania do administrativo e Cássio do site, estou super satisfeita com a empresa e recomendo a todos.
+                                    Muito obrigada!</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Slide 3-->
+                <div class="carousel-item">
+                    <div class="div-depoimentos">
+                        <div class="card-depoimentos">
+                            <div class="depoimentos-header">
+                                <img src="https://via.placeholder.com/150" alt="">
+                                <div>
+                                    <p>Miliete</p>
+                                    <p>Corretora autônoma</p>
+                                </div>
+
+                            </div>
+                            <div class="depoimentos-body">
+                                <p>Quando comecei a trabalhar como profissional autônoma, queria me profissionalizar, no ramo Imobiliário e estar nos melhores portais. Então não tive dúvidas, escolhi o melhor! Midas. Os profissionais desse sistema, são atenciosos e realmente resolvem sempre o que precisamos. Neste ramo, não podemos perder tempo e o Midas, nós dá um excelente suporte. Parabéns a equipe Midas. </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!-- indicadores laterais do carrossel de imagens -->
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="span-carrossel" aria-hidden="true">
+                    < </span>
+                        <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="span-carrossel" aria-hidden="true"> > </span>
+                <span class="sr-only">Next</span>
+            </a>
+
+
+        </div>
+    </div>
+
+    <!--  -->
+    <!--  -->
+
+    <!-- fimm da area de depoimentos -->
+
+    <!-- area de contato -->
+    <div class="form-contato">
+        <div class="div-texto-formulario">
+            <p>O FUTURO DA SUA IMOBILIÁRIA COMEÇA AGORA!</p>
+        </div>
+        <div class="div-formulario">
+            <p>Preencha o formulário e receba uma oferta exclusiva.</p>
+            <form action="">
+                <div class="div-field">
+                    <label for="nome">Nome Completo<span style="color: red;">*</span></label>
+                    <input type="text" name="nome" id="input-nome">
+                </div>
+                <div class="div-field">
+                    <label for="imobiliaria">Imobiliária<span style="color: red;">*</span></label>
+                    <input type="text" name="imobiliaria" id="input-imobiliaria">
+                </div>
+                <div class="div-field">
+                    <label for="e-mail">E-mail<span style="color: red;">*</span></label>
+                    <input type="text" name="e-mail" id="input-e-mail">
+                </div>
+                <div class="div-field">
+                    <label for="telefone">Telefone<span style="color: red;">*</span></label>
+                    <input type="text" name="telefone" id="input-telefone">
+                </div>
+                <div class="div-field">
+                    <label for="mensagem">Mensagem<span style="color: red;">*</span></label>
+                    <textarea rows="4" name="mensagem" id="input-mensagem">Olá, tenho interesse em saber mais sobre a locação digital. Aguardo contato.</textarea>
+                </div>
+
+            </form>
+        </div>
+    </div>
+
+    <div class="rodape">
+        <div>
+            <h3 class="texto-centralizado">MIDAS MAIS</h3>
+            <p class="texto-centralizado">As soluções MIDAS vem deixando sua marca de sucesso em cada uma das Empresas que procuram utilizar o máximo da tecnologia em favor dos seus negócios.</p>
+        </div>
+    </div>
+
+
+
+    <!--  -->
+    <!--  -->
+    <!--  -->
+    <!-- Modal -->
+    <div class="modal fade" id="modalBaner" tabindex="-1" role="dialog" aria-labelledby="modalBanerLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalBanerLabel">Titulo da campanha</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <img src="img/fundo2.jpg" alt="">
+                </div>
+                <div class=" modal-footer">
+                    <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button> -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script type="text/javascript">
+        /**Ativa o modal */
+        // $(window).on('load', function() {
+        //     $('#modalBaner').modal('show');
+        // });
+    </script>
+</body>
+
+
+
+</html>
