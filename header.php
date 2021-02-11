e24dd80db1ee6ee1db446bea69c9b975d80fe75f
viniciusrad <36489527+viniciusrad@users.noreply.github.com>
2021-02-11 10:25:56 -0300

    meta tag adequada a chrome

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, target-densityDpi=device-dpi" />
+
 
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
@@ -51,8 +53,8 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto barra-enderecos">
-                    <li class="nav-item">
+                <ul class="navbar-nav ml-auto barra-enderecos list-none" style="list-style-type: none;">
+                    <li style="list-style: none;" class="nav-item">
                         <a href="./index.php">Início</a>
                     </li>
                     <li class="nav-item">
