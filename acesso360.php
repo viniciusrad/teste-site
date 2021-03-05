feb8e570e8ae0ccd492f090738f145765d3cf6df
viniciusrad <36489527+viniciusrad@users.noreply.github.com>
2021-03-05 10:20:35 -0300

    nova pagina criada

---
+++ acesso360.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+
+<?php include('header.php') ?>
+
+<body>
+    <!--  -->
+    <!--  -->
+    <div class="area-acesso-midas360">
+
+        <a href="https://www.midas360.com.br/midas360/login.php">Login </a>
+        |
+        <a href="https://midas360.imb.br/midas360/cadastro.phpp"> Cadastro</a>
+    </div>
+
+    <div class="logo-midas-grande">
+        <img src="img/Logo-Midas-360-Colorida--1024x493.jpg" alt="Logo Midas">
+    </div>
+
+    <div class="imagem-principal-midas360">
+        <img src="img/imagem-topo-360.jpg" alt="Imagem simulação tour virtual">
+    </div>
+
+    <div class="div-cinza-grande">
+        Ofereça visitas online 24 horas.
+    </div>
+
+
+    <div class="container-caracteristicas-midas360">
+
+        <div class="card-icone card-icone-360">
+            <div class="div-icone">
+                <i class="fas fa-desktop font-icone-midas360"></i>
+            </div>
+            <div class="container">
+                <p class="textos-icones-360">SISTEMA INTEGRADO</p>
+            </div>
+            <p class="paragrafo-icones-360">Melhore o tráfego para o seu site e converta mais clientes. Páginas com fotos e Tours Virtuais 360° têm o dobro de probabilidade de gerar interesse nos visitantes.</p>
+        </div>
+
+        <div class="card-icone card-icone-360">
+            <div class="div-icone">
+                <i class="fas fa-star font-icone-midas360"></i>
+            </div>
+            <div class="container">
+                <p class="textos-icones-360">EXCLUSIVIDADE</p>
+            </div>
+            <p class="paragrafo-icones-360">Seus imóveis online, abertos e disponíveis 24 horas por dia, sem precisar reservar as chaves! Obtenha dos proprietários exclusividade com a visita virtual.</p>
+        </div>
+
+        <div class="card-icone card-icone-360">
+            <div class="div-icone">
+
+
+                <i class="fas fa-check font-icone-midas360"></i>
+
+            </div>
+            <div class="container">
+                <p class="textos-icones-360">ASSERTIVIDADE</p>
+            </div>
+            <p class="paragrafo-icones-360">Visita presencial? Apenas nos imóveis que o cliente tiver real interesse. A visita virtual dá ao cliente a real sensação de estar dentro do imóvel.</p>
+        </div>
+
+        <div class="card-icone card-icone-360">
+            <div class="div-icone">
+                <i class="far fa-handshake font-icone-midas360"></i>
+            </div>
+            <div class="container">
+                <p class="textos-icones-360">RESULTADO</p>
+                <p class="paragrafo-icones-360">Os compradores e locatários se sentirão mais confortáveis pois não há necessidade de locomoção e agendamento. Grandes chances de fechamento.</p>
+            </div>
+        </div>
+
+    </div>
+    <!--  -->
+    <!--  -->
+
+
+    <div class="div-secao-midas360" style="font-size: 1.3rem;">
+        <div class="div-texto-midas360">
+            <h3>CONHEÇA A VISITA VIRTUAL</h3>
+            </br>
+            <p>A “experiência 360º” está se tornando o padrão de apresentação de imóveis e negócios por todo o mundo. </p>
+            </br>
+            <p>Se manter antenado com as novas tecnologias é fundamental para se destacar diante de um cenário extremamente competitivo.</p>
+            </br>
+            <p>Se você vende ou aluga imóveis, reduza o tempo e os custos com visitas físicas.</p>
+            <p>Permita que seu cliente visite seu espaço de onde estiver e quando quiser. </p>
+
+        </div>
+        <div class="div-iframe-midas360">
+            <iframe width="100%" height="475" src="https://midas360.imb.br/visita_virtual/visita.php?i=MTMw&amp;b=MTk5OQ&amp;t=ae69de19a42c962a1bf99627753eb71a" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+        </div>
+        <div class="div-iframe-mobile">
+            <iframe width="100%" height="475" src="https://midas360.imb.br/visita_virtual/visita.php?i=MTMw&amp;b=MTk5OQ&amp;t=ae69de19a42c962a1bf99627753eb71a" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+        </div>
+    </div>
+
+
+    <div class="div-secao-midas360" style="font-size: 1.3rem; background-color: #e2e2e2;">
+        <div class="div-texto-midas360">
+            <h4>COMO CRIAR UMA VISITA VIRTUAL</h4>
+            </br>
+
+            <p>Fácil, rápido e intuitivo.</p>
+
+        </div>
+        <div class="div-video-midas360">
+            <iframe class="video-iframe" allowfullscreen="" title="Player de Vídeo youtube" src="https://www.youtube.com/embed/dpfEF4sJf-w?feature=oembed&amp;start&amp;end&amp;wmode=opaque&amp;loop=0&amp;controls=1&amp;mute=0&amp;rel=0&amp;modestbranding=0"></iframe>
+        </div>
+
+    </div>
+
+    <div class="div-secao-midas360" style="font-size: 1.3rem;">
+        <div class="div-texto-midas360">
+            <h4>COMO TIRAR FOTOS EM 360°</h4>
+            </br>
+
+            <p>Veja como tirar fotos em 360 é mais simples e rápido do que tirar fotos em seu celular.</p>
+
+        </div>
+        <div class="div-video-midas360">
+            <iframe class="elementor-video-iframe" allowfullscreen="" title="Player de Vídeo youtube" src="https://www.youtube.com/embed/OODeBwRWCcw?feature=oembed&amp;start&amp;end&amp;wmode=opaque&amp;loop=0&amp;controls=1&amp;mute=0&amp;rel=0&amp;modestbranding=0"></iframe>
+        </div>
+
+    </div>
+
+    <div class="div-secao-midas360" style="font-size: 1.3rem; background-color: #e2e2e2;">
+        <div class="div-texto-midas360">
+            <h4>COMO GERAR FOTOS PLANAS</h4>
+            </br>
+
+            <p>O único do mercado capaz de gerar fotos planas a partir do tour 360.</p>
+
+        </div>
+        <div class="div-video-midas360">
+            <iframe class="elementor-video-iframe" allowfullscreen="" title="Player de Vídeo youtube" src="https://www.youtube.com/embed/H913MZqySWI?feature=oembed&amp;start&amp;end&amp;wmode=opaque&amp;loop=0&amp;controls=1&amp;mute=0&amp;rel=0&amp;modestbranding=0"></iframe>
+        </div>
+
+    </div>
+
+
+    <div class="div-texto-centralizado">
+        <i aria-hidden="true" class="fas fa-camera" style="font-size: 4rem; margin: 2rem; color: #15ced8"></i>
+        <br>
+        <p>No mercado existem diversas opções de câmeras que geram as imagens em 360 para montar o tour, registrando fotos e vídeos de todas as direções ao mesmo tempo. Os valores também são variados, mas é importante antes de investir em um modelo analisar sua funcionalidade. Como sugestão indicamos as camêras "Samsung Gear 360" e "Câmera 360 Ricoh Theta".</p>
+    </div>
+
+    <div class="logo-midas-grande">
+        <img src="img/Logo-Midas-360-Colorida--1024x493.jpg" alt="Logo Midas">
+    </div>
+
+    <div class="div-area-cinza"></div>
+
+    <!-- area de depoimentos -->
+    <!--  -->
+    <?php
+    include('area-depoimentos.php')
+    ?>
+    <!-- fimm da area de depoimentos -->
+
+    <!-- area de contato -->
+
+    <?php
+    include('area-contato.php')
+    ?>
+    <!-- FIM da area de contato -->
+
+    <!-- rodapé -->
+    <?php
+    include('area-rodape.php')
+    ?>
+
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#input-mensagem').val('Olá, tenho interesse em saber mais sobre o Chatbot. Aguardo contato.')
+        });
+    </script>
+</body>
+
+
+
+</html>
