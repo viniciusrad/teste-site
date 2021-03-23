af522dab9ca6a438454742bd19def1c85c773a32
viniciusrad <36489527+viniciusrad@users.noreply.github.com>
2021-03-23 15:17:33 -0300

    description tag div exibe contato googleAds

--- header.php
+++ header.php
@@ -41,7 +41,7 @@ $telefoneComercial = $telefonesVindosArquivo[$indexTelefone];
     <meta charset="UTF-8">
     <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, target-densityDpi=device-dpi" />
-
+    <meta name="description" content="<?php echo $descricao ?>" />
 
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
@@ -64,6 +64,17 @@ $telefoneComercial = $telefonesVindosArquivo[$indexTelefone];
 
     <!-- Global site tag (gtag.js) - Google Ads: 874740353 -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=AW-874740353"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', 'AW-874740353');
+    </script>
 
 
     <title><?php echo $tituloPagina ?></title>
@@ -136,7 +147,11 @@ $telefoneComercial = $telefonesVindosArquivo[$indexTelefone];
             <div class="div-social-icons">
                 <a class="" href="https://www.instagram.com/midas_mais/"><i class="fas fa-camera"></i></a>
                 <a class="" href="https://www.facebook.com/MidasSoftImob"><i class="fab fa-facebook"></i></a>
-                <a class="" href="contato.php"><i class="fas fa-phone-alt"></i></a>
+                <a class="" id="btn-contato-ligar"><i class="fas fa-phone-alt"></i></a>
+                <div class="pop-telefone aux-display-none">
+                    <p>Telefone para contato:</p>
+                    <p> (21) 2214-0113</p>
+                </div>
             </div>
             <!--  -->
             <!--  -->
@@ -200,7 +215,10 @@ $telefoneComercial = $telefonesVindosArquivo[$indexTelefone];
 
     $(document).ready(function() {
 
-
+        $('#btn-contato-ligar').click(() => {
+            //alert("clicou")
+            $(".pop-telefone").toggleClass('aux-display-none')
+        })
 
     })
 </script>
