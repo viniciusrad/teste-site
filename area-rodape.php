c5cf8c11350895b7d2f924820270e012b3ee3de3
viniciusrad <36489527+viniciusrad@users.noreply.github.com>
2021-04-09 15:53:16 -0300

    retorno do link do whatsapp

--- area-rodape.php
+++ area-rodape.php
@@ -5,6 +5,13 @@
             <p class="texto-centralizado">As soluções MIDAS vem deixando sua marca de sucesso em cada uma das Empresas que procuram utilizar o máximo da tecnologia em favor dos seus negócios.</p>
         </div>
 
+        <!-- whatsappp link -->
+        <div class="container-whatsapp">
+            <a class="contato-telefone-whatsapp" href="http://api.whatsapp.com/send?1=pt_BR&phone=55<?php echo $telefoneComercial; ?>" target="_blank">
+                <i class="fa fa-whatsapp icone-whatsapp"></i>
+            </a>
+        </div>
+
     </div>
 </footer>
 
