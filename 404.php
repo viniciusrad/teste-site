e025742b18088e8dd2736e8cbc17f60cd90f6897
viniciusrad <36489527+viniciusrad@users.noreply.github.com>
2021-04-20 11:57:01 -0300

    incluido arquivo 404.php

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php
+$pagina_titulo  = "Página Não Encontrada";
+$descricao      = "Ops..";
+include_once "header.php"; ?>
+<section>
+  	<div class="container-fluid text-center">
+    	<div class="row">
+      		<div class="col-xs-12 primeiro-item col-centered text-center">
+      			<h1 class="text-center" style="text-align:center !important; margin-top: 15%; padding-bottom: 50px;"><span style="font-size: 70px; padding: 20px;    display: inline-block;">Ops...</span><br>Esta página não foi encontrada</h1>
+      			<a href="/" class="btn btn-lg">Voltar</a>
+  			</div>
+  		</div>
+  	</div>
+</section>
+<?php include_once "footer.php"; ?>
